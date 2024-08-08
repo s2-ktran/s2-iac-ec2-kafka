fffc5fd829927954447410920028e17f16c0187b
Kevin Tran <ktran@singlestore.com>
2024-08-08 14:41:19 -0700

    feat: destroy script now works

--- teardown.sh
+++ teardown.sh
@@ -6,6 +6,16 @@ set -e
 # Prompt for the AWS region
 read -p "Enter the AWS region (e.g., us-east-1): " AWS_REGION
 
+# Fetch your current public IP address
+MY_IP=$(curl -s https://api.ipify.org)
+
+# TODO: Make this automated
+# Define your SingleStore IP addresses
+SINGLE_STORE_IPS="52.39.174.38/32,44.230.94.214/32,35.81.56.251/32,52.35.160.52/32"
+
+# Convert the comma-separated IP addresses to a format acceptable by Terraform
+SINGLE_STORE_IPS_LIST=$(echo $SINGLE_STORE_IPS | sed 's/,/","/g' | sed 's/^/["/' | sed 's/$/"]/')
+
 # Validate the region
 if [[ -z "$AWS_REGION" ]]; then
   echo "Region cannot be empty."
@@ -13,9 +23,9 @@ if [[ -z "$AWS_REGION" ]]; then
 fi
 
 # Initialize Terraform with the specified region
-terraform init -var "region=${AWS_REGION}"
+terraform init -var "region=${AWS_REGION}" -var "instance_type=${INSTANCE_TYPE}"
 
-# Destroy the Terraform-managed infrastructure
-terraform destroy -var "region=${AWS_REGION}" -auto-approve
+# Apply Terraform configuration to create resources
+terraform destroy -var "my_ip=${MY_IP}/32" -var "single_store_ips=${SINGLE_STORE_IPS_LIST}" -var "region=${AWS_REGION}" -var "instance_type=${INSTANCE_TYPE}" -auto-approve
 
-echo "Terraform infrastructure has been destroyed."
+echo "Infrastructure destroyed successfully."
